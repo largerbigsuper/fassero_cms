de12ab77d183a9fb09be227c68cdd54045421f7b
Frankie <zaihuazhao@163.com>
2020-01-17 01:04:25 +0800

    add django settings

--- apps/users/api/viewsets.py
+++ apps/users/api/viewsets.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.conf import settings
 
 
 from ..models import mm_User
