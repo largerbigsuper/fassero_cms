d40844581fad4781e9e5e56415071acf02d4fb5b
Frankie <zaihuazhao@163.com>
2020-01-20 22:54:08 +0800

    增加过滤参数

--- apps/cfg/api/filters.py
+++ apps/cfg/api/filters.py
@@ -7,5 +7,6 @@ class ArticleFilter(filters.FilterSet):
     class Meta:
         model = Article
         fields = {
-            'site_module': ['exact']
+            'site_module': ['exact'],
+            'site_module__code': ['exact']
         }
